05c990e66b0b0db0a177fc7512d366320c6d78ea
Salvatore Capolupo <s.capolupo@gmail.com>
2026-04-05 18:06:30 +0200

    Update duplicate-radar.php

--- duplicate-radar.php
+++ duplicate-radar.php
@@ -7,7 +7,8 @@
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Salvatore Capolupo
- * License:           MIT License
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       duplicate-radar-main
  */
 
